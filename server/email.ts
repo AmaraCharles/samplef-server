e6766eb5236aa9d49b768cbff911334d4ece21ea
AmaraCharles <falsepegasus@gmail.com>
2026-01-22 11:01:39 +0100

    tags

--- server/email.ts
+++ server/email.ts
@@ -4,7 +4,7 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY || "");
-const FROM_EMAIL ="support@Blocktoart.com"
+const FROM_EMAIL ="support@blocktoart.com"
   // process.env.FROM_EMAIL || "Blocktoart <support@Blocktoart.com>";
 
 // ----------------------------
